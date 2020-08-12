7c7384387074de28c94db852c5c1cd86653c1031
Francisco Rosas <rosaf2@student.op.ac.nz>
2020-08-12 19:41:35 +1200

    Initial commit, Tutorial Completed

---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center mt-5">
+    <div class="container">
+        This is a footer    
+    </div>
+</footer>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>@yield('title','Laravel 5.8 Basics')</title>
+    <style>
+        .alert{
+            z-index: 99;
+            top: 60px;
+            right:18px;
+            min-width:30%;
+            position: fixed;
+            animation: slide 0.5s forwards;
+        }
+        @keyframes slide {
+            100% { top: 30px; }
+        }
+        @media screen and (max-width: 668px) {
+            .alert{ /* center the alert on small screens */
+                left: 10px;
+                right: 10px; 
+            }
+        }
+        </style>
+</head>
+<body>
+
+    {{-- That's how you write a comment in blade --}}
+
+    @include('inc.navbar')
+
+    <main class="container mt-4">
+        @yield('content')
+    </main>
+
+    @include('inc.footer')
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+    @if(session('status')) {{-- <- If session key exists --}}
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{session('status')}} {{-- <- Display the session value --}}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif
+
+    <script>
+        //close the alert after 3 seconds.
+        $(document).ready(function(){
+        setTimeout(function() {
+            $(".alert").alert('close');
+        }, 3000);
+        });
+    </script>
+</body>
+</html>
